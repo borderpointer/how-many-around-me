ce1ea5336c9134d3d08ec9ac4f618fdf47a3ae7c
borderpointer <ellijpark@gmail.com>
2016-03-23 16:38:19 -0400

    need to work on ajax calls for dynamic updating

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,37 +17,62 @@
 //= require bootstrap-sprockets
 
 $(function () {
-    $('#chart-container').highcharts({
+  Highcharts.setOptions({
+      chart: {
+          type: 'line',
+          plotBackgroundColor: {
+            linearGradient: [0, 0, 500, 500],
+            stops: [
+                [0, 'rgb(150, 125, 255)'],
+                [1, 'rgb(255, 100, 10)']
+            ]
+          },
+          style: {
+              fontFamily: '"Montserrat", Helvetica, sans-serif',
+              fontSize: '16px'
+          },
+          marginTop: 50
+      }
+  });
+
+  $.ajax({
+    type: "GET",
+    url: "/api/cars",
+    dataType: 'json',
+    success: function(data){
+      $('#chart-container').highcharts({
         title: {
-            text: 'Number of Available Uber Cars Near My Apartment',
+          text: 'Number of Available Uber Cars Near My Apartment',
             x: -20 //center
         },
         xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+          categories: [dateFormat(data[0].created_at, "dddd, mmmm dS, yyyy, h:MM:ss TT")]
         },
         yAxis: {
-            title: {
-                text: 'Number of Cars'
+          title: {
+            text: 'Number of Cars'
             },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
+          plotLines: [{
+            value: 0,
+            width: 1,
             }]
         },
         tooltip: {
-            valueSuffix: 'Cars'
+          valueSuffix: ' Cars'
         },
         legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'middle',
+          borderWidth: 0
         },
         series: [{
-            name: ' ',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+          name: ' ',
+          color: '#f6f6f6',
+          data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
         }]
-    });
+      });
+      // options.series[0].setData(data);
+    }
+  });
 });
